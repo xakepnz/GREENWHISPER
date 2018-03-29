428ba59af09d5003f7eba55c3ef363828c0bf304
xakep <maximizemc@gmail.com>
2018-03-30 10:50:40 +1300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Name: install.sh
+# Description: Installer for ubuntu/debian flavour linux distro's.
+
+# Install strings
+sudo apt-get install binutils -y -q=2
+
+# Install exiftool
+sudo apt-get install exiftool -y -q=2
